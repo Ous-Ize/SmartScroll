066945c5b5d6f1a0cbe4f18b8dacb7e3b8ef19d9
Ahmadkhdeir <ahmadkhdier@icloud.com>
2025-02-01 08:29:53 +0100

    changes on welcome page

--- react-native-app/app/index.jsx
+++ react-native-app/app/index.jsx
@@ -9,24 +9,22 @@ export default function App() {
   return (
     <SafeAreaView className="bg-background h-full">
       <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
-        <View className="flex-1 items-center px-6 py-[180]">
+        <View className="flex-1 items-center px-6 py-[280]">
           <Image 
             source={images.logo} 
             resizeMode="contain" 
             className="px-4 w-[200px] h-[200px]"
           />
           <Text 
-            className="px-4 text-4xl text-black font-bold text-center mt-4">
+            className="px-4 text-4xl text-black font-bold text-center mt-4 mb-20">
               Welcome to Smart Scroll
             </Text>
             <CustomButton
             title="Continue with Email"
             handlePress={() => router.push('/sign-in')}
-            containerStyles="w-full mt-10" 
+            containerStyles="w-full" 
           />
-        </View>
-        <View className="px-6">
-          <Text className="text-center font-semibold">
+          <Text className="text-center font-semibold py-[40]">
             Smart Scroll can make mistakes! check important information.
           </Text>
         </View>
