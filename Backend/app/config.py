65b05ce75a37a487cac9bacdda879d22fe6bc21f
Ahmadkhdeir <ahmadkhdier@icloud.com>
2025-03-02 21:33:06 +0100

    added end-point for the sign-in page

--- Backend/app/config.py
+++ Backend/app/config.py
@@ -5,7 +5,7 @@ class Config:
     DB_NAME = "PPR_WiSe24_001"
     DEBUG = True
     UPLOAD_FOLDER = "pdfs"
-    OPENAI_API_KEY = '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' # Add your API key 
+    OPENAI_API_KEY = '' # Add your API key 
 
 
 config = Config()
